d143345eb9d6a85d2c83557de1f511b01d9dc598
K1Tpro <uwarovkirill@gmail.com>
2024-05-20 11:15:40 -0500

    1

--- Task_4/main.py
+++ Task_4/main.py
@@ -5,9 +5,32 @@ def parse_input(user_input):
 
 
 def add_contact(args, contacts):
-    name, phone = args
-    contacts[name] = phone
-    return "Contact added."
+    try:
+        name, phone = args
+        contacts[name] = phone
+        return "Contact added."
+    except ValueError:
+        return "Incorrect amount of values. Please enter name and number"
+
+
+def show_phone(args, contacts):
+    name = args[0]
+    if name in contacts:
+        return f"Phone number for {name} is {contacts.get(name)}"
+    else:
+        return f"There are no {name} in your contacts"
+
+
+def change_contact(args, contacts):
+    try:
+        name, new_phone = args
+        if name in contacts:
+            contacts[name] = new_phone
+            return "Contact updated"
+        else:
+            return f"There are no {name} in your contacts. Type add for the new contact"
+    except ValueError:
+        return "Incorrect amount of values. Please enter name and number"
 
 
 def main():
@@ -24,6 +47,10 @@ def main():
             print("How can I help you?")
         elif command == "add":
             print(add_contact(args, contacts))
+        elif command == "show":
+            print(show_phone(args, contacts))
+        elif command == "update":
+            print(change_contact(args, contacts))
         else:
             print("Invalid command.")
 
